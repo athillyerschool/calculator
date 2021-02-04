02f2be642d3811e80fd08e45967400bbee541015
Allan Hillyer <hillyeal@oregonstate.edu>
2021-02-04 09:10:18 -0800

    file with calculator functions

---
+++ calculator.py
@@ -0,0 +1,11 @@
+
+def add(num1, num2):
+    return float(float(num1) + float(num2))
+def subtract(num1, num2):
+    return float(float(num1) - float(num2))
+def multiply(num1, num2):
+    return float(float(num1) * float(num2))
+def divide(num1, num2):
+    if(num2 == 0):
+        return None
+    return float(float(num1) / float(num2))
